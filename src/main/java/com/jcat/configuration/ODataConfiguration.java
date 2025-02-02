71a2f5dafb37ade2bce2d86b9dee6a348590509a
Mr.Anderson <Follow the White Rabbit>
2025-02-02 23:53:52 +0300

    some fixes

--- src/main/java/com/jcat/configuration/ODataConfiguration.java
+++ src/main/java/com/jcat/configuration/ODataConfiguration.java
@@ -2,12 +2,18 @@ package com.jcat.configuration;
 
 import com.jcat.properties.ApplicationProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.olingo.client.api.ODataClient;
 import org.apache.olingo.client.core.ODataClientFactory;
-import org.apache.olingo.client.core.http.BasicAuthHttpClientFactory;
+import org.apache.olingo.client.core.http.DefaultHttpClientFactory;
 import org.apache.olingo.commons.api.format.ContentType;
+import org.apache.olingo.commons.api.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @Slf4j
@@ -18,12 +24,24 @@ public class ODataConfiguration {
         ODataClient client = ODataClientFactory.getClient();
         org.apache.olingo.client.api.Configuration configuration = client.getConfiguration();
         configuration.setDefaultPubFormat(ContentType.APPLICATION_JSON);
+
+        final String auth = Base64.encodeBase64String(
+            String.format("%s:%s",
+                    applicationProperties.getC1().getOData().getUser(),
+                    applicationProperties.getC1().getOData().getPassword())
+                .getBytes(StandardCharsets.UTF_8));
+
         configuration.setHttpClientFactory(
-            new BasicAuthHttpClientFactory(
-                applicationProperties.getC1().getOData().getUser(),
-                applicationProperties.getC1().getOData().getPassword()
-            )
-        );
+            new DefaultHttpClientFactory() {
+                @SuppressWarnings("deprecation")
+                @Override
+                public DefaultHttpClient create(HttpMethod method, URI uri) {
+                    final DefaultHttpClient client = super.create(method, uri);
+                    client.addRequestInterceptor((request, context) -> request.addHeader("Authorization", "Basic " + auth));
+                    return client;
+                }
+            });
+
         return client;
     }
 
